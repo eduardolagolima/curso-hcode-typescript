6dd0de38cc6c60a073ad79d76b3a84b6fd0ce7eb
eduardolagolima <eduardolagolima@gmail.com>
2021-08-03 15:46:09 -0300

    adiciona exemplo de union-types

---
+++ build/2-types/union-types.js
@@ -0,0 +1,9 @@
+"use strict";
+var pixKey;
+pixKey = 'teste@teste.teste';
+console.log(pixKey);
+pixKey = 54987654321;
+console.log(pixKey);
+pixKey = '01234567890';
+console.log(pixKey);
+//# sourceMappingURL=union-types.js.map
---
+++ src/2-types/union-types.ts
@@ -0,0 +1,16 @@
+let pixKey: string | number;
+
+// ? email
+pixKey = 'teste@teste.teste';
+
+console.log(pixKey);
+
+// ? phone
+pixKey = 54987654321;
+
+console.log(pixKey);
+
+// ? cpf
+pixKey = '01234567890';
+
+console.log(pixKey);
