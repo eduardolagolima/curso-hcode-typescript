45e7033f7c61c3d8e01da56eaa745372fbb45d1a
eduardolagolima <eduardolagolima@gmail.com>
2021-08-04 13:06:23 -0300

    adiciona exemplo com os tipos null e undefined

---
+++ build/2-types/null-undefined.js
@@ -0,0 +1,8 @@
+"use strict";
+var db;
+db = 'postgresql';
+db = null;
+var version;
+version = '13.3';
+version = undefined;
+//# sourceMappingURL=null-undefined.js.map
---
+++ src/2-types/null-undefined.ts
@@ -0,0 +1,9 @@
+let db: string | null;
+
+db = 'postgresql';
+db = null;
+
+let version: string | undefined;
+
+version = '13.3';
+version = undefined;
