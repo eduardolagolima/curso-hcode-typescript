4f542395722d8fa7a93d5e6e16676c72de054e97
eduardolagolima <eduardolagolima@gmail.com>
2021-08-03 21:16:11 -0300

    adiciona exemplo com o tipo tuple

---
+++ build/2-types/tuple.js
@@ -0,0 +1,9 @@
+"use strict";
+var user;
+user = [
+    'João',
+    'teste@teste.teste',
+    32,
+    false,
+];
+//# sourceMappingURL=tuple.js.map
---
+++ src/2-types/tuple.ts
@@ -0,0 +1,10 @@
+// ? "Nome", "E-mail", "Idade", "Administrador?"
+let user: [string, string, number, boolean];
+
+// * Valida os tipos, a ordem e a quantidade dos itens
+user = [
+    'João',
+    'teste@teste.teste',
+    32,
+    false,
+];
