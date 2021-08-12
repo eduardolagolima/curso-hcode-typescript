c899ec3c67485e330e11754792a7dbfec41cfde1
Eduardo Lago Lima <eduardolagolima@gmail.com>
2021-08-12 15:07:22 -0300

    adiciona funcionalidade que informa os números no visor da calculadora

--- project/src/calculator/CalculatorController.ts
+++ project/src/calculator/CalculatorController.ts
@@ -6,5 +6,40 @@ export default class CalculatorController {
         private screen = new Screen()
     ) {
         new DateTime();
+        this.buttonEvents();
+    }
+
+    buttonEvents(): void {
+        document.querySelectorAll('#teclado button').forEach((element) => {
+            element.addEventListener('click', (event: Event) => {
+                const target = event.target as HTMLButtonElement;
+
+                if (target.className === 'numero') {
+                    this.addNumber(Number(target.dataset.valor));
+                } else if (target.id === 'adicao') {
+
+                } else if (target.id === 'subtracao') {
+
+                } else if (target.id === 'divisao') {
+
+                } else if (target.id === 'multiplicacao') {
+
+                } else if (target.id === 'ponto') {
+
+                } else if (target.id === 'limpar') {
+
+                } else if (target.id === 'desfazer') {
+
+                } else if (target.id === 'porcentagem') {
+
+                } else if (target.id === 'igual') {
+
+                }
+            });
+        });
+    }
+
+    addNumber(number: number): void {
+        this.screen.value = number.toString();
     }
 }
